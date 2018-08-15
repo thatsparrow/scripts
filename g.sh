6271cec0b9d27443c4f866e20190f87166ba39fc
thatsparrow <jbenjamin001@gmail.com>
2018-08-15 14:47:50 +0530

    google headline in select online media

--- g.sh
+++ g.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# This script checks for online coverage
 
 if [[ $(echo $*) ]]; then
 
@@ -6,10 +7,16 @@ if [[ $(echo $*) ]]; then
 
 else
 
-    read -p "Enter your search term: " searchterm
+    read -p "Enter the headline: " searchterm
 
 fi
 
 searchterm=$(echo $searchterm | sed -e 's/\ /+/g')
 
-lynx -dump http://www.google.com/search?q=$searchterm | less
+lynx -dump http://www.google.com/search?q=$searchterm+lankapuvath.lk | less
+lynx -dump http://www.google.com/search?q=$searchterm+adaderana.lk | less
+lynx -dump http://www.google.com/search?q=$searchterm+cbr.lk | less
+lynx -dump http://www.google.com/search?q=$searchterm+asianmirror.lk | less
+lynx -dump http://www.google.com/search?q=$searchterm+lankabusinessnews.lk | less
+lynx -dump http://www.google.com/search?q=$searchterm+asianmirror.lk | less
+lynx -dump http://www.google.com/search?q=$searchterm+techadvisor.lk | less
